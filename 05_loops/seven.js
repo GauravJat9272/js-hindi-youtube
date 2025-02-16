3717defce8cc3d96d41b7ba8cbe646237755b50e
GauravJat9272 <gjaat9272@gmail.com>
2025-02-16 12:10:51 +0000

    reduce method

--- 05_loops/seven.js
+++ 05_loops/seven.js
@@ -13,3 +13,26 @@ const myNums = [1,2,3]
 //in arrow f/n
 const myTotal = myNums.reduce( (acc,curr) => acc+curr,0)
 console.log(myTotal); //6
+
+
+const shoppingCart =[ 
+    {
+    itemName: "js course",
+    price: 2999
+    },
+    {
+    itemName: "python course",
+    price: 999
+    },
+    {
+    itemName: "Web course",
+    price: 5999
+    },
+    {
+    itemName: "AI course",
+    price: 12999
+    }
+]
+
+const priceToPay = shoppingCart.reduce( (acc,item) => acc+item.price, 0)
+console.log(priceToPay); //22996
